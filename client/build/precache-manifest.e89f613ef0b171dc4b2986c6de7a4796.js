9e6e0c573f5ca9f55d56f09a0ce4f9abf2e392a8
jdang217 <jdang217@vt.edu>
2021-12-23 03:26:31 -0500

    MERN boilerplate

---
+++ client/build/precache-manifest.e89f613ef0b171dc4b2986c6de7a4796.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d113f319446fbfc28f086746b6bbf423",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2214363d2d344deb18fe",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "401e745065819688c86e",
+    "url": "/static/js/2.e7b1de21.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.e7b1de21.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2214363d2d344deb18fe",
+    "url": "/static/js/main.019299e4.chunk.js"
+  },
+  {
+    "revision": "4387b907a4d64303b5a2",
+    "url": "/static/js/runtime-main.fab42d54.js"
+  }
+]);
