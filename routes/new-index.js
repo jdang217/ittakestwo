147d192325af1c3cb65b16d5600451441f9a6f15
jdang217 <jdang217@vt.edu>
2021-12-23 03:26:31 -0500

    MERN boilerplate

---
+++ routes/new-index.js
@@ -0,0 +1,23 @@
+
+var express = require('express');
+var router = express.Router();
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "***REMOVED***";
+
+
+router.get('/', (req, res, next) => {
+	MongoClient.connect(url, function(err, db) {
+		if (err) throw err;
+		var dbo = db.db("ItTakesTwoDB");
+		dbo.collection("collection1").find({}).toArray(function(err, result) {
+			if (err) throw err;
+			console.log('Mongo data coming in hot')
+    		console.log(result);
+    		res.json(result)
+    		db.close();
+    	});
+	}); 
+});
+
+module.exports = router;
