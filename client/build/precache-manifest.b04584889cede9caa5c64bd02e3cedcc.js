517a7a8022558eaf9804e11f2333160caad06629
jdang217 <jdang217@vt.edu>
2021-12-23 15:32:54 -0500

    added react routing skeleton

---
+++ client/build/precache-manifest.b04584889cede9caa5c64bd02e3cedcc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44c65cef6a9c1ca7f890bf4d9b132ec2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a921b441af5938d426d3",
+    "url": "/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "9f01389268c75c83fe58",
+    "url": "/static/css/main.8a44e726.chunk.css"
+  },
+  {
+    "revision": "a921b441af5938d426d3",
+    "url": "/static/js/2.550279fa.chunk.js"
+  },
+  {
+    "revision": "4ab48ad028ea4629a492f79ee58fd6d0",
+    "url": "/static/js/2.550279fa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9f01389268c75c83fe58",
+    "url": "/static/js/main.fa3642b5.chunk.js"
+  },
+  {
+    "revision": "2a52619c1c1d54aae417",
+    "url": "/static/js/runtime-main.1f937d7b.js"
+  }
+]);
---
+++ startserver.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#server to compile react and start server
+cd client
+npm run build
+cd ..
+npm start
